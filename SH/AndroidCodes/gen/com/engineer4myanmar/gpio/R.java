3c8a7b5e207246968152776bb172f4a2e14c3689
Dr.Aung Win Htut <aungwinnhtut@gmail.com>
2015-10-24 13:01:25 +0630

    version 0.02

--- SH/AndroidCodes/gen/com/engineer4myanmar/gpio/R.java
+++ SH/AndroidCodes/gen/com/engineer4myanmar/gpio/R.java
@@ -11,8 +11,11 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int gl=0x7f020000;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_launche1r=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int ic_launcher1=0x7f020004;
     }
     public static final class id {
         public static final int menu_settings=0x7f070011;
